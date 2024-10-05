b8257e4b76b64ee39abd02fa1f2dd73bdf7eb5ae
Lucas Chicco <55957303+lucaschicco@users.noreply.github.com>
2024-10-04 21:11:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -48,11 +48,11 @@ app.index_string = '''
 '''
 
 # Cargar datos
-file_path = 'https://jsonbuscafe.blob.core.windows.net/contbuscafe/base_todos_barrios_vf2.xlsx'
+file_path = 'https://jsonbuscafe.blob.core.windows.net/contbuscafe/base_todos_barrios_vf3.xlsx'
 data = pd.read_excel(file_path)
 
 # URL of the JSON file
-url = 'https://jsonbuscafe.blob.core.windows.net/contbuscafe/geojson_data2.json'
+url = 'https://jsonbuscafe.blob.core.windows.net/contbuscafe/geojson_data3.json'
 # Fetch the content from the URL
 response = requests.get(url)
 # Load the content into a Python dictionary
